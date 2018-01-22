2b51f4abf75afda59c9add167d423af94eb0bf37
Magdiel Campelo Alves de Sousa <magdiel.camp@gmail.com>
2018-01-21 23:53:45 -0300

    currency list actions

--- app/actions/currencies.js
+++ app/actions/currencies.js
@@ -1,6 +1,8 @@
 export const actions = {
   SWAP_CURRENCY: 'SWAP_CURRENCY',
   CHANGE_CURRENCY_AMOUNT: 'CHANGE_CURRENCY_AMOUNT',
+  CHANGE_BASE_CURRENCY: 'CHANGE_BASE_CURRENCY',
+  CHANGE_QUOTE_CURRENCY: 'CHANGE_QUOTE_CURRENCY',
 };
 
 export const swapCurrency = () => ({
@@ -10,4 +12,14 @@ export const swapCurrency = () => ({
 export const changeCurrencyAmount = amount => ({
   type: actions.CHANGE_CURRENCY_AMOUNT,
   amount: parseFloat(amount),
+});
+
+export const changeBaseCurrency = currency => ({
+  type: actions.CHANGE_BASE_CURRENCY,
+  currency,
+});
+
+export const changeQuoteCurrency = currency => ({
+  type: actions.CHANGE_QUOTE_CURRENCY,
+  currency,
 });
--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FlatList, View, StatusBar } from 'react-native';
+import { connect } from 'react-redux';
 
 import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
 const TEMP_CURRENT_CURRENCY = 'CAD';
 
 class CurrencyList extends Component {
   static propTypes = {
     navigation: PropTypes.object,
+    dispatch: PropTypes.func,
   }
 
-  handlePress = () => {
+  handlePress = (currency) => {
+    const { type } = this.props.navigation.state.params;
+    if (type === 'base') {
+      this.props.dispatch(changeBaseCurrency(currency));
+    } else if (type === 'quote') {
+      this.props.dispatch(changeQuoteCurrency(currency));
+    }
     this.props.navigation.goBack(null);
-    console.log("press element");
   };
 
   render() {
@@ -30,7 +38,7 @@ class CurrencyList extends Component {
             <ListItem
               text={item}
               selected={item === TEMP_CURRENT_CURRENCY}
-              onPress={this.handlePress}
+              onPress={() => this.handlePress(item)}
             />
           }
           keyExtractor={item => item}
@@ -41,4 +49,4 @@ class CurrencyList extends Component {
   }
 }
 
-export default CurrencyList;
+export default connect()(CurrencyList);
